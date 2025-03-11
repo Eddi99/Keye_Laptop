6a9a437c2af1bdb22bc268d7a16210ec022d6a59
Eddi99 <103059527+Eddi99@users.noreply.github.com>
2025-03-11 10:09:36 +0100

    Setup.bar aktualisiert, gitignore angepasst, sodass requirements hochgeaden werden

--- setup.bat
+++ setup.bat
@@ -1,16 +1,16 @@
 @echo off
 echo [1/5] Erstelle virtuelle Umgebung...
-python -m venv env || echo Fehler beim Erstellen der virtuellen Umgebung! & pause & exit
+python -m venv env && echo Virtuelle Umgebung erstellt.
 
 echo [2/5] Aktiviere virtuelle Umgebung...
-call env\Scripts\activate.bat || echo Fehler beim Aktivieren der virtuellen Umgebung! & pause & exit
+call env\Scripts\activate.bat && echo Virtuelle Umgebung aktiviert.
 
 echo [3/5] Installiere Abhängigkeiten...
-pip install --upgrade pip || echo Fehler beim Upgrade von pip! & pause & exit
-pip install -r requirements.txt || echo Fehler beim Installieren der Abhängigkeiten! & pause & exit
+pip install --upgrade pip && echo Pip aktualisiert.
+pip install -r requirements.txt && echo Abhängigkeiten installiert.
 
 echo [4/5] Erstelle ausführbare Datei...
-pyinstaller main.spec || echo Fehler beim Erstellen der EXE-Datei! & pause & exit
+pyinstaller main.spec && echo EXE-Datei erstellt.
 
 echo [5/5] Erstelle Verknüpfung auf dem Desktop...
 powershell -command "$s=(New-Object -COM WScript.Shell).CreateShortcut(\"$env:USERPROFILE\Desktop\Keye.lnk\"); $s.TargetPath=\"%CD%\dist\Keye.exe\"; $s.IconLocation=\"%CD%\icon.ico\"; $s.Save()"
